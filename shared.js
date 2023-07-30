4688ee5cbfdeef0a2686ec388a4454345dd95bde
wostrau <a.ostrovtsov@gmail.com>
2023-07-30 17:13:02 +0200

    clean up code: added latest features to packages and customers pages

--- shared.js
+++ shared.js
@@ -7,24 +7,36 @@ const mobileNav = document.querySelector('.mobile-nav');
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', () => {
-        backdrop.style.display = 'block';
-        modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        // modal.className = 'open' // This will actually overwrite the complete class list
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     });
 }
 
 const closeModal = () => {
-    backdrop.style.display = 'none';
-    modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    if (modal) {
+        modal.classList.remove('open');
+    }
+    backdrop.classList.remove('open');
 };
 
 backdrop.addEventListener('click', () => {
-    mobileNav.style.display = 'none';
+    //mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
     closeModal();
 });
 
-modalNoButton.addEventListener('click', closeModal);
+if (modalNoButton) {
+    modalNoButton.addEventListener('click', closeModal);
+}
 
 toggleButton.addEventListener('click', () => {
-    mobileNav.style.display = 'block';
-    backdrop.style.display = 'block';
+    //mobileNav.style.display = 'block';
+    //backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
 });
